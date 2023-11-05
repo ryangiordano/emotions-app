ff83ce2d0a48b3eaa979f7b7673f22eff05e9ba2
thedaruma <rgiorda1@gmail.com>
2023-11-05 14:40:14 -0500

    add random util

--- src/components/face/animations/transition.ts
+++ src/components/face/animations/transition.ts
@@ -1,4 +1,5 @@
 import anime from "animejs";
+import { getRandomBetween } from "../../../utils/random";
 
 export function transitionInUp(
   elements: NodeListOf<Element> | Element | NodeListOf<Element>[],
@@ -7,13 +8,13 @@ export function transitionInUp(
   anime({
     targets: elements,
     opacity: [0, 1],
-    translateY: [100, 0],
+    translateY: [getRandomBetween(-100, 100), 0],
     scaleX: [0, 1],
     duration: 500,
     easing: "easeInOutBack",
     loop: false,
     direction: "alternate",
-    delay: anime.stagger(50), // increase delay by 100ms for each elements.
+    delay: anime.stagger(25), // increase delay by 100ms for each elements.
     complete: onComplete,
   });
 }
