a305c1a78907298c10e42cb75d30fceab44a3a1b
Ryan Giordano <ryangiordano@Ryans-MacBook-Air.local>
2023-12-25 22:17:18 -0500

    add juice to animations for journal list

--- src/pages/user-info/JournalList.tsx
+++ src/pages/user-info/JournalList.tsx
@@ -3,24 +3,50 @@ import { Link } from "react-router-dom";
 import "./journal-list.scss";
 import { emotionColorMap } from "../../components/constants";
 import { Emotions } from "../../components/face/constants";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function JournalList({ journals }: { journals: any[] }) {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+    },
+  };
+
+  const listItemVariants = {
+    hidden: { opacity: 0, y: -20 },
+    show: { opacity: 1, y: 0 },
+  };
   return (
-    <div className="journal-list">
-      {journals.map((doc) => {
-        const data = doc.data();
-        return (
-          <Link
-            to={`/journal/${doc.id}`}
-            key={doc.id}
-            className={`journal-list-item ${
-              emotionColorMap[data.emotion as Emotions]
-            }`}
-          >
-            {data.text}
-          </Link>
-        );
-      })}
-    </div>
+    <motion.ul
+      className="journal-list"
+      variants={containerVariants}
+      initial="hidden"
+      animate="show"
+      exit={{ opacity: 0 }}
+    >
+      <AnimatePresence mode="wait">
+        {journals.map((doc, i) => {
+          const data = doc.data();
+          return (
+            <motion.li
+              key={doc.id}
+              variants={listItemVariants}
+              exit={{ y: 20, opacity: 0 }}
+              transition={{ duration: 0.08, delay: i * 0.08 }}
+            >
+              <Link
+                to={`/journal/${doc.id}`}
+                className={`journal-list-item ${
+                  emotionColorMap[data.emotion as Emotions]
+                }`}
+              >
+                {data.text}
+              </Link>
+            </motion.li>
+          );
+        })}
+      </AnimatePresence>
+    </motion.ul>
   );
 }
