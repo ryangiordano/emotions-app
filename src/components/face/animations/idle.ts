1c3d6a7364e774136a66de5c013b164f01ca3829
Ryan Giordano <ryangiordano@Ryans-MacBook-Air.local>
2023-12-30 07:23:43 -0500

    return idle animation

--- src/components/face/animations/idle.ts
+++ src/components/face/animations/idle.ts
@@ -1,14 +1,14 @@
 import anime from "animejs";
 
 export function hover(body: NodeListOf<Element> | Element) {
-  // anime({
-  //   targets: body,
-  //   translateY: [0, 20],
-  //   duration: 1000,
-  //   easing: "easeInOutSine",
-  //   loop: true,
-  //   direction: "alternate",
-  // });
+  anime({
+    targets: body,
+    translateY: [0, 20],
+    duration: 1000,
+    easing: "easeInOutSine",
+    loop: true,
+    direction: "alternate",
+  });
 }
 
 export function shake(body: NodeListOf<Element> | Element) {
